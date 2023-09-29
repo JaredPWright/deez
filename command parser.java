2c77f9f1c082519e625b7274d67c3cd2c9b8ebd0
MDog37127 <bentleymdog@gmail.com>
2023-09-29 11:43:21 -0500

    Create command parser.java

---
+++ command parser.java
@@ -0,0 +1,158 @@
+public class parser {
+    
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < args.length; i++)
+        {
+            String line = args[i];
+            if(line.equals("SRSCAN") || line.equals("S"))
+            {
+
+            }
+            else if(line.equals("STATUS") || line.equals("ST"))
+            {
+
+            }
+            else if(line.equals("LRSCAN") || line.equals("L"))
+            {
+                
+            }
+            else if(line.equals("CHART") || line.equals("C"))
+            {
+                
+            }
+            else if(line.equals("DAMAGES") || line.equals("DA"))
+            {
+                
+            }
+            else if(line.equals("MOVE") || line.equals("M"))
+            {
+                
+            }
+            else if(line.equals("WARP") || line.equals("W"))
+            {
+                
+            }
+            else if(line.equals("IMPUSLE") || line.equals("I"))
+            {
+                
+            }
+            else if(line.equals("SHIELDS") || line.equals("SH"))
+            {
+                
+            }
+            else if(line.equals("PHASERS") || line.equals("P"))
+            {
+                
+            }
+            else if(line.equals("CLOAK"))
+            {
+                
+            }
+            else if(line.equals("CAPTURE") || line.equals("CA"))
+            {
+                
+            }
+            else if(line.equals("REPORT") || line.equals("REP"))
+            {
+                
+            }
+            else if(line.equals("COMPUTER") || line.equals("CO"))
+            {
+                
+            }
+            else if(line.equals("TORPEDO") || line.equals("TO"))
+            {
+                
+            }
+            else if(line.equals("DOCK") || line.equals("D"))
+            {
+                
+            }
+            else if(line.equals("REST") || line.equals("R"))
+            {
+                
+            }
+            else if(line.equals("MAYDAY"))
+            {
+                
+            }
+            else if(line.equals("ABONDON"))
+            {
+                
+            }
+            else if(line.equals("DESTRUCT"))
+            {
+                
+            }
+            else if(line.equals("QUIT"))
+            {
+                
+            }
+            else if(line.equals("SENSORS") || line.equals("SE"))
+            {
+                
+            }
+            else if(line.equals("ORBIT") || line.equals("O"))
+            {
+                
+            }
+            else if(line.equals("TRANSPORT") || line.equals("T"))
+            {
+                
+            }
+            else if(line.equals("SHUTTLE") || line.equals("SHU"))
+            {
+                
+            }
+            else if(line.equals("MINE") || line.equals("MI"))
+            {
+                
+            }
+            else if(line.equals("STATUS") || line.equals("ST"))
+            {
+                
+            }
+            else if(line.equals("CRYSTALS") || line.equals("CR"))
+            {
+                
+            }
+            else if(line.equals("PLANETS") || line.equals("PL"))
+            {
+                
+            }
+            else if(line.equals("FREEZE"))
+            {
+                
+            }
+            else if(line.equals("REQUEST") || line.equals("REQ"))
+            {
+                
+            }
+            else if(line.equals("DEATHRAY"))
+            {
+                
+            }
+            else if(line.equals("PROBE") || line.equals("PR"))
+            {
+                
+            }
+            else if(line.equals("EMEXIT") || line.equals("E"))
+            {
+                
+            }
+            else if(line.equals("HELP"))
+            {
+                
+            }
+            else if(line.equals("CURSES") || line.equals("CURSES"))
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
