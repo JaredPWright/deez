389b984ba2bdc7b372c83579db706514caa41e63
Jared Wright <jaredwright.ok@gmail.com>
2023-10-30 14:27:16 -0500

    made some general changes. Like adding a tick function

--- src/main/java/SST/Star.java
+++ src/main/java/SST/Star.java
@@ -1,9 +1,30 @@
 package SST;
 
-public class Star {
-    public char icon = 'S';
+import java.util.List;
 
-    public Star(){
+public class Star extends ObjectCore {
+    public char icon = 'S';
+    private MapDesignation starDesignation;
+    private Quadrant parentQuadrant;
+    private List<Star> adjacentStars;
+    private boolean novaed = false;
+
+    public Star(int x, int y, Quadrant parent){
         icon = 'S';
+        starDesignation = new MapDesignation(x,y);
+        parentQuadrant = parent;
+    }
+
+    public void Nova(/*Player player*/){
+        //if(isInRange(1, starDesignation, player) && !novaed)
+        //  damagePlayer();
+        gatherLocalStars(/*player*/);
+    }
+
+    private void gatherLocalStars(/*Player player*/){
+        for(int i = 0; i < parentQuadrant.GetStarList().size(); i++){
+            //if(isInRange(1, starDesignation, parentQuadrant.GetStarList().get(i).starDesignation))
+            //  parentQuadrant.GetStarList().get(i).Nova(player);
+        }
     }
 }
