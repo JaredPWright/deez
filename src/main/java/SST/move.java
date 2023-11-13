886f8390ea9e00331a5314afd425f34aaa003987
MDog37127 <94014195+MDog37127@users.noreply.github.com>
2023-11-13 14:41:25 -0600

    changes to phaser and move

--- src/main/java/SST/move.java
+++ src/main/java/SST/move.java
@@ -1,62 +1,61 @@
 package SST;
 
-public class move
-{
+public class Move {
     public static int movement = 1;
-    public static void main(String[] args)
-    {
-        for(int i = 0; i < args.length; i++)
-        {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
             String line = args[i];
-            if(line.equals("FOWARD"))
-            {
-                int warp = WARP();
 
+            if (line.equals("FORWARD")) {
+                int warp = getWarp();
                 int move = movement * warp;
-
                 int sec = Player.getSector();
-                //int qua = Player.getQuadrant();
 
-                Player.setSector() = move - sec;
-                //when player added, find the cords in the nested for loop and move the player up x places
+                // Assuming setSector is a method in the Player class
+                Player.setSector(sec + move);
+
+                // When player is added, find the coordinates in the nested for loop and move the player up x places
             }
-            if(line.equals("Right"))
-            {
-                int warp = WARP();
 
+            if (line.equals("RIGHT")) {
+                int warp = getWarp();
                 int move = movement * warp;
-
                 int qua = Player.getQuadrant();
 
-                Player.setQuadrant = move + qua;
-                //when player added, find the cords in the nested for loop and move the player right x places
+                // Assuming setQuadrant is a method in the Player class
+                Player.setQuadrant(qua + move);
+
+                // When player is added, find the coordinates in the nested for loop and move the player right x places
             }
-            if(line.equals("Left"))
-            {
-                int warp = WARP();
 
+            if (line.equals("LEFT")) {
+                int warp = getWarp();
                 int move = movement * warp;
-
                 int qua = Player.getQuadrant();
 
-                Player.setQuadrant = move - qua;
+                // Assuming setQuadrant is a method in the Player class
+                Player.setQuadrant(qua - move);
 
-                //when player added, find the cords in the nested for loop and move the player left x places
+                // When player is added, find the coordinates in the nested for loop and move the player left x places
             }
-            if(line.equals("Backward"))
-            {
-                int warp = WARP();
 
+            if (line.equals("BACKWARD")) {
+                int warp = getWarp();
                 int move = movement * warp;
-
                 int sec = Player.getSector();
-                //int qua = Player.getQuadrant();
 
-                Player.setSector() = move + sec;
+                // Assuming setSector is a method in the Player class
+                Player.setSector(sec - move);
 
-                //when player added, find the cords in the nested for loop and move the player down x places
+                // When player is added, find the coordinates in the nested for loop and move the player down x places
             }
         }
     }
 
+    private static int getWarp() {
+        // Implement the logic for obtaining the warp factor
+        // This might involve user input or a predefined value
+        return 1; // Placeholder, replace with actual logic
+    }
 }
--- src/main/java/SST/phaser.java
+++ src/main/java/SST/phaser.java
@@ -1,21 +1,43 @@
 package SST;
 
-public class phaser {
+public class Phaser {
+    private int energy;
+    private String target;
 
-    boolean hit()
-    {
-        int playerx = Player.getSector();
-        int playery = Player.getQuadrant();
+    public Phaser(int initialEnergy) {
+        this.energy = initialEnergy;
+        this.target = null; // No initial target
+    }
 
-        int enemyx = enemy.getSector();
-        int ememyy = enemy.getQuadrant();
+    public void setTarget(String target) {
+        this.target = target;
+        System.out.println("Target set to: " + target);
+    }
 
-        if(playerx == enemyx)
-        {
-            hit = true;
-        } else if (playery == enemyy) {
-            hit = true;
+    public void fire() {
+        if (energy > 0 && target != null) {
+            int damage = calculateDamage();
+            System.out.println("Phaser fired at " + target + "! Damage inflicted: " + damage);
+        } else if (target == null) {
+            System.out.println("No target set. Unable to fire.");
+        } else {
+            System.out.println("Phaser out of energy. Unable to fire.");
         }
-        return hit;
+    }
+
+    private int calculateDamage() {
+        // Implement your damage calculation logic here
+        // This might depend on various factors such as distance, shield strength, etc.
+        // For simplicity, let's assume a basic calculation for demonstration purposes
+        return Math.min(energy, 50); // Placeholder value, replace with actual logic
+    }
+
+    public void recharge(int amount) {
+        energy += amount;
+        System.out.println("Phaser recharged. Current energy: " + energy);
+    }
+
+    public int getEnergy() {
+        return energy;
     }
 }
