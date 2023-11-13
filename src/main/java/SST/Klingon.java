0a452bbf3a91a9e3e3574a51b6c58ffa09f56396
MDog37127 <94014195+MDog37127@users.noreply.github.com>
2023-11-13 14:50:44 -0600

    added klingon

---
+++ src/main/java/SST/Klingon.java
@@ -0,0 +1,29 @@
+package SST;
+
+public class Klingon {
+    private String name;
+    private int energy;
+
+    public Klingon(String name, int initialEnergy) {
+        this.name = name;
+        this.energy = initialEnergy;
+    }
+
+    public void takeDamage(int damage) {
+        energy -= damage;
+        if (energy <= 0) {
+            System.out.println(name + " has been defeated!");
+        } else {
+            System.out.println(name + " takes damage. Remaining energy: " + energy);
+        }
+    }
+
+    public void attack(Player player) {
+        // Implement Klingon's attack logic here
+        // For simplicity, let's assume a basic attack for demonstration purposes
+        int damage = 20; // Placeholder value, replace with actual logic
+        player.takeDamage(damage);
+    }
+
+    // Other methods and properties specific to Klingons can be added here
+}
